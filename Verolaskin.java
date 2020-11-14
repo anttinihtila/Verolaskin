75fa9cbdd9e16f195f12674694198bc31843e974
Antti Nihtilä <68847740+anttinihtila@users.noreply.github.com>
2020-11-14 13:21:21 +0200

    Add files via upload
    Verolaskin. Java ohjelma.

---
+++ Verolaskin.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import javax.swing.*;
+import javax.swing.JOptionPane;
+
+public class Verolaskin {
+
+    public static void main(String[] args) {
+        String num1, num2;
+        float numero1, numero2, vero, veroton;
+
+        num1 = JOptionPane.showInputDialog(null, "Hinta ?", "Verolaskin", JOptionPane.QUESTION_MESSAGE);
+        numero1 = Float.parseFloat(num1);
+
+        num2 = JOptionPane.showInputDialog(null, "Veroprosentti ?", "Verolaskin", JOptionPane.QUESTION_MESSAGE);
+        numero2 = Float.parseFloat(num2);
+
+        vero=numero1*numero2/100;
+        veroton=numero1-vero;
+
+        JOptionPane.showMessageDialog(null, "Vero: " + vero +" e" , "Verolaskin", JOptionPane.PLAIN_MESSAGE );
+        JOptionPane.showMessageDialog(null, "Veroton hinta : " + veroton +" e" , "Verolaskin", JOptionPane.PLAIN_MESSAGE );
+        
+        System.exit(0);
+    }
+}
